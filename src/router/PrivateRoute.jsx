ff693f5914689f1b85728ed0a73cb7828b134ed5
abusayedsohagislam <abusayedsohagislam25@gmail.com>
2025-04-24 00:46:04 +0600

    add private route

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { AuthContext } from './Provider/AuthContext';
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+
+    const { user } = useContext(AuthContext);
+
+    return user ? children : <Navigate to="/login" />;
+
+};
+
+export default PrivateRoute;
